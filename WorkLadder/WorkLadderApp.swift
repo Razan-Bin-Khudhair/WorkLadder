8e0c8a06fe454b6e9beae120b3de2c3a42c05042
razan <Razan1939@gmail.com>
2024-01-25 02:56:03 +0300

    Initial Commit

---
+++ WorkLadder/WorkLadderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkLadderApp.swift
+//  WorkLadder
+//
+//  Created by Razan Bin Khudhair on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkLadderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
